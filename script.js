feffdf3beddb698f38210f9f48e24ec5096d7b23
Yaroslav Nepochatov <yaroslav.n18@gmail.com>
2025-05-05 19:32:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let miners = document.querySelector("miner_types")
+let qna = document.querySelector("QnA")
+let support = document.querySelector("support")
+miners.addEventListener("click", function(){
+    console.log("A")
+})
+qna.addEventListener("click", function(){
+    console.log("B")
+})
+support.addEventListener("click", function(){
+    console.log("C")
+})
 
