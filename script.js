df17ee9648f55190a4610b7c5ab2e46c813f42ab
ObamaGaming-69420 <yaroslav.n18@gmail.com>
2025-05-05 19:15:09 +0300

    Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let miners = document.querySelector("miner_types")
+let qna = document.querySelector("QnA")
+let support = document.querySelector("support")
+miners.addEventListener("click", function(){
+    console.log("A")
+})
+qna.addEventListener("click", function(){
+    console.log("B")
+})
+support.addEventListener("click", function(){
+    console.log("C")
+})
